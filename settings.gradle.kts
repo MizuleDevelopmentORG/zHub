82ec06e3113455e70f1394115c7f34dfe3994b53
powercas_gamer <cas@deltapvp.net>
2023-08-21 02:21:31 +0200

    fix: wrong settings file

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,24 +17,4 @@ plugins {
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
-dependencyResolutionManagement {
-    // repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        mavenCentral()
-        maven("https://maven.deltapvp.net")
-    }
-}
-
-rootProject.name = "licenser-parent"
-
-// Make sure to update bom/build.gradle.kts when making changes to modules.
-
-sequenceOf(
-    "bom",
-    "common",
-    "paper",
-    "api"
-).forEach {
-    include("licenser-$it")
-    project(":licenser-$it").projectDir = file(it)
-}
+rootProject.name = "zHub"
